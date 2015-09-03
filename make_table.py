f7480b44fb5f8239ec9b69f4da490089ca1a84f3
Tim Leverentz <timlev@gmail.com>
2015-09-02 22:22:21 -0500

    initial commit

---
+++ make_table.py
@@ -0,0 +1,69 @@
+import csv, os, download_dict_sound
+csvfilename = "3rd grade words.csv"
+title = csvfilename[:csvfilename.rfind(".")]
+htmlfilename = title + ".html"
+
+if os.path.isdir(title) == False:
+    os.mkdir(title)
+
+#GET WORDS
+data = []
+with open(csvfilename, "r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        data.append(row)
+
+#Form HTML BLOB
+htmlblob = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>""" + title + """</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.23.1" />
+	<link rel="stylesheet" type="text/css" href='../css_file.css'>
+</head>
+
+<body>
+<div><img align="left" src="../click.png" width="75" height="75"></img><h2>Click to listen to the words.</h2></div>
+<div>"""
+
+htmlblob += "<table border=1>\n"
+for row in data:
+    htmlblob += "<tr>\n"
+    for item in [x for x in row if x != ""]:
+		htmlblob += "<td>\n"
+		htmlblob += """\t<audio src='""" + item + """.mp3' id='"""+ item + """'> \n"""
+		htmlblob += """\t</audio>\n"""
+		htmlblob += """\t<div id='""" + item + """_text' onClick="document.getElementById('""" + item + """').play();">""" + item + """</div>\n"""
+		htmlblob += "</td>\n"
+    htmlblob += "</tr>\n"
+
+htmlblob += "</table>\n"
+htmlblob += """</div>
+</body>
+
+</html>"""
+with open(os.path.join(title, htmlfilename), "w") as htmlfile:
+    htmlfile.write(htmlblob)
+
+#MAKE LIST OF WORDS TO DOWNLOAD
+toDownload = []
+for row in data:
+    for item in [x for x in row if x != ""]:
+        toDownload.append(item)
+
+#DOWNLOAD WORDS
+not_downloaded = []
+
+for word in toDownload:
+    #print word
+    if download_dict_sound.download(word, os.path.relpath(title)) != 0:
+        not_downloaded.append(word)
+print "Not downloaded:\n"
+print not_downloaded
+
+#WRITE FILE WITH LIST OF NOT DOWNLOADED
+with open(os.path.join(title, "not_downloaded.txt"),"w") as fp:
+    fp.write("\n".join(not_downloaded))
